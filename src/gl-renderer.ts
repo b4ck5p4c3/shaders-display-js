791dfb6bf594eb9ccde42e3b64437de555005d34
Egor Koleda <radioegor146@ctf.by>
2024-04-13 03:23:32 +0300

    fix

--- src/gl-renderer.ts
+++ src/gl-renderer.ts
@@ -137,7 +137,7 @@ export class GLRenderer {
         for (let y = 0; y < this.height; y++) {
             data.push([]);
             for (let x = 0; x < this.width; x++) {
-                data[y].push(buffer[(y * this.width + (this.width - x - 1)) * 4]);
+                data[y].push(buffer[((this.height - y - 1) * this.width + x) * 4]);
             }
         }
 
